f06c78fb7b326b6a9a0a09e7fcf994caa2453323
Ashis Kumar Dey <ashisdey128@gmail.com>
2023-11-04 13:21:33 +0530

    updated profile page

--- my-profile/src/app/components/project-experience/project-experience.component.ts
+++ my-profile/src/app/components/project-experience/project-experience.component.ts
@@ -18,17 +18,28 @@ export class ProjectExperienceComponent implements OnInit {
 
   slideConfig = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 3000,
     responsive: [
+      {
+        breakpoint: 1367,
+        settings: {
+          prevArrow: null,
+          nextArrow: null,
+          dots: true
+        }
+      },
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
-          infinite: true,
+          prevArrow: null,
+          nextArrow: null,
           dots: true
         }
       },
@@ -36,14 +47,20 @@ export class ProjectExperienceComponent implements OnInit {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 2,
+          prevArrow: null,
+          nextArrow: null,
+          dots: true
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          prevArrow: null,
+          nextArrow: null,
+          dots: true
         }
       }
       // You can unslick at a given breakpoint now by adding:
@@ -75,6 +92,8 @@ export class ProjectExperienceComponent implements OnInit {
       this.projloading = false;
     });
   }
+
+
 
 
 
